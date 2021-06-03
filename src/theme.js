20006153409260a96256c5cb536de97a03792d4e
Subroto   Biswas <37511552+toofaniCoder@users.noreply.github.com>
2021-06-03 16:56:31 +0530

    add global theming options

--- src/theme.js
+++ src/theme.js
@@ -4,6 +4,20 @@ const theme = createMuiTheme({
   palette: {
     type: "dark",
   },
+  overrides: {
+    // Style sheet name ⚛️
+    // Write component name for style overrides
+    
+    MuiCssBaseline: {
+      '@global': {
+       // wirte global styling here
+      },
+    },
+  },
+  // set default props of any component
+  props: {
+    // Name of the component ⚛️
+  },
 });
 
 export default theme;
